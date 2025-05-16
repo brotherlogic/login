717b2730c268677564a98c78b1b681d6779af85c
Simon Tucker <brotherlogic@gmail.com>
2025-05-15 21:14:25 -0700

    Cleared test

--- loginapi_test.go
+++ loginapi_test.go
@@ -4,31 +4,18 @@ import (
 	"context"
 	"testing"
 
-	"github.com/brotherlogic/keystore/client"
+	keystoreclient "github.com/brotherlogic/keystore/client"
 
 	pb "github.com/brotherlogic/login/proto"
 )
 
-//InitTestServer gets a test version of the server
+// InitTestServer gets a test version of the server
 func InitTestServer() *Server {
 	s := Init()
 	s.SkipLog = true
 	s.GoServer.KSclient = *keystoreclient.GetTestClient(".test")
 	s.GoServer.KSclient.Save(context.Background(), CONFIG, &pb.Config{})
 	return s
-}
-
-func TestAllFail(t *testing.T) {
-	s := InitTestServer()
-	_, err := s.Login(context.Background(), &pb.LoginRequest{})
-	if err == nil {
-		t.Errorf("Oh oh")
-	}
-	_, err = s.Authenticate(context.Background(), &pb.AuthenticateRequest{})
-	if err == nil {
-		t.Errorf("Oh oh")
-	}
-
 }
 
 func TestSetToken(t *testing.T) {
